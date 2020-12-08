c5c170118c42dc67c353b7b9a8f669e58afe998e
410713071 <410713071@users.noreply.github.com>
2020-12-08 11:07:28 +0800

    Initial commit

---
+++ Assets/Script/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody playerrb;
+    public int upforce = 100;
+    private bool isOnGround = true; // on ground
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerrb = GetComponent<Rigidbody>();
+        playerrb.AddForce(Vector3.up * upforce);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround)
+        {
+            playerrb.AddForce(Vector3.up * upforce, ForceMode.Impulse);
+            isOnGround = false; // not on ground
+        }
+
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        isOnGround = true; // on ground
+    }
+}
