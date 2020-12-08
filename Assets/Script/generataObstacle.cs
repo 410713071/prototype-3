8d32f2469e1a9e98d4be35af6fefa716ac5a04d8
410713071 <410713071@users.noreply.github.com>
2020-12-08 12:04:02 +0800

    1208done

---
+++ Assets/Script/generataObstacle.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class generataObstacle : MonoBehaviour
+{
+    public GameObject obstaclePrefab;
+    private float start_dealy = 1.0f;
+    private float repetate_rate = 0.5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("Gobstacles", start_dealy, repetate_rate);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void Gobstacles()
+    {
+        // generate obstacle prefab
+        Vector3 obstacle_pos = new Vector3(25, 0, 0);
+        Instantiate(obstaclePrefab, obstacle_pos, obstaclePrefab.transform.rotation);
+    }
+}
---
+++ Assets/Script/moveBackground.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moveBackground : MonoBehaviour
+{
+    private Vector3 startpos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        startpos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.x < startpos.x - 50)
+        {
+            transform.position = startpos;
+        }
+
+    }
+}
